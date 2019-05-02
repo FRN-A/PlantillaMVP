611524c9dd5f835d2c72a84cb823dca880e392fe
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-05-02 09:47:07 -0700

    Agregar archivos de proyecto.

---
+++ PlantillaOpenGL/Carro.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Modelo.h"
+#include "glm/gtx/transform.hpp"
+#include "glm/glm.hpp"
+
+class Carro : public Modelo {
+public:
+	Carro();
+	void avanzar();
+	void rotar();
+private: 
+	vec3 coordenadas = vec3(0.0f, 0.0f, 0.0f);
+	void actualizarMatrizModelo();
+};
